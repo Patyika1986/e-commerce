cd38baeff1af68a1facd5088716107e51eb698c3
Peter <peter.lakatos@outlook.de>
2023-05-30 18:54:56 +0200

    add default overview router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './component/nav/nav.component';
+import { OverviewComponent } from './component/overview/overview.component';
 
 const routes: Routes = [
+  { path: 'overview',component: OverviewComponent},
+  { path: '',redirectTo:'overview',pathMatch:'full'},
+  { path: '', component: NavComponent},
   {
     path: 'data-store',
     loadChildren: () =>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './component/nav/nav.component';
+import { OverviewComponent } from './component/overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
